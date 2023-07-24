96d4efbb0f116bcf2d404e8f0d5d9acbdd886fc2
Jonas Andersen <94622735+jonasanders1@users.noreply.github.com>
2023-07-24 16:30:24 +0200

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import Movie from './components/Movie';
+import Filter from './components/Filter';
+
+function App() {
+
+  const [popular, setPopular] = useState([]);
+
+  useEffect(()=> {
+    fetchPopular();
+  }, []);
+
+
+  const fetchPopular = async () => {
+    const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=419ccf4e6df574acc9c99e54abfd2a04&en-US&page=1`)
+    const movies = await data.json()
+    setPopular(movies.results)
+  }
+
+  return (
+    <div>
+      <Filter />
+    <div className='popular-movies'>
+      {popular.map((movie) => {
+        return <Movie key={movie.id} movie={movie} />
+      })}
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Filter.jsx
@@ -0,0 +1,13 @@
+import {useEffect, useState} from 'react'
+
+const Filter = () => {
+  return (
+    <div className='filter-continer'>
+      <button>All</button>
+      <button>Comedy</button>
+      <button>Action</button>
+    </div>
+  )
+}
+
+export default Filter
